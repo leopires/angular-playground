403d9dd57af994fb9ca22c794100780f8e828981
Leonardo Pires <lbpires@gmail.com>
2021-10-29 09:44:04 -0300

    - Atualização das bibliotcas; - Adição do Angular Material UI.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {ListsModule} from "./lists/lists.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {ListsModule} from "./lists/lists.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ListsModule
+    ListsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
