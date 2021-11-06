001092382cad571b85eb7d1c31eb5c754785538c
Leonardo Pires <lbpires@gmail.com>
2021-11-06 17:29:42 -0300

    Home component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {ListsModule} from "./lists/lists.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatToolbarModule,
     ListsModule,
     BrowserAnimationsModule
   ],
